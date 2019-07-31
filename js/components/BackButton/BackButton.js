dfc133ba697d7c4c4accac466a8927ed5daf1066
Emre Kaynak <emrekaynak94@gmail.com>
2019-07-31 15:19:53 +0300

    Use arrow icon for back button

--- js/components/BackButton/BackButton.js
+++ js/components/BackButton/BackButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/AntDesign";
 import PropTypes from "prop-types";
 import styles from "./styles";
 import theme from "../../theme";
@@ -8,11 +8,7 @@ import theme from "../../theme";
 const BackButton = ({ onPress }) => (
   <TouchableOpacity onPress={() => onPress()}>
     <View>
-      <Icon
-        name="md-arrow-round-back"
-        size={25}
-        color={theme.palette.primary.main}
-      />
+      <Icon name="arrowleft" size={25} color={theme.palette.primary.main} />
     </View>
   </TouchableOpacity>
 );

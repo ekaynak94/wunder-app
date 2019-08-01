2b953c0505cf09f92664de459a884b5dff940eba
Emre Kaynak <emrekaynak94@gmail.com>
2019-08-01 17:49:21 +0300

    Re-init socket connection after expiry

--- js/context/UsersContext/UsersContext.js
+++ js/context/UsersContext/UsersContext.js
@@ -1,21 +1,31 @@
 import React, { Component, createContext } from "react";
-
 const UsersContext = createContext();
 
 class UsersProvider extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      socketOpen: false
     };
-    this.socket = new WebSocket(
-      "wss://wunder-provider.herokuapp.com/socket.io/?EIO=3&transport=websocket"
-    );
+    this.socket = null;
   }
 
   componentDidMount() {
+    this.initSocket();
+  }
+
+  componentDidUpdate() {
+    if (!this.state.socketOpen) this.initSocket();
+  }
+
+  initSocket() {
+    this.socket = new WebSocket(
+      "wss://wunder-provider.herokuapp.com/socket.io/?EIO=3&transport=websocket"
+    );
+    this.socket.onopen = () => this.setState({ socketOpen: true });
+    this.socket.onclose = () => this.setState({ socketOpen: false });
     this.socket.onmessage = ({ data }) => {
-      console.log(data);
       const match = data.match(/{"results":\[.*\],"info":\{.*\}\}/);
       if (match) {
         const json = JSON.parse(match[0]);

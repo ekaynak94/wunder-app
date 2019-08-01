42d6c7b990a2d4f15e554570b84d913902eeeb13
Emre Kaynak <emrekaynak94@gmail.com>
2019-08-01 14:41:56 +0300

    Use web socket to get data from output stream

--- js/context/UsersContext/UsersContext.js
+++ js/context/UsersContext/UsersContext.js
@@ -1,14 +1,25 @@
 import React, { Component, createContext } from "react";
 import usersData from "../../mock/users.json";
+import console = require("console");
 
 const UsersContext = createContext();
 
 class UsersProvider extends Component {
   constructor() {
     super();
-    this.state = { users: usersData.results };
+    this.state = {
+      users: usersData.results,
+      open: false
+    };
+    this.socket = new WebSocket(
+      "wss://wunder-provider.herokuapp.com/socket.io/?EIO=3&transport=websocket"
+    );
   }
 
+  componentDidMount() {
+    this.socket.onmessage = ({ data }) => console.log(data)
+  }
+  r;
   render() {
     return (
       <UsersContext.Provider value={this.state.users}>

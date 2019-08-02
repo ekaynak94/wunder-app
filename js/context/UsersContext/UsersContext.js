9460471d033aeb0c39cb42d5c1b11651168b2688
Emre Kaynak <emrekaynak94@gmail.com>
2019-08-02 08:51:42 +0300

    Install and use react-native-websocket to establish connection with outputstream

--- js/context/UsersContext/UsersContext.js
+++ js/context/UsersContext/UsersContext.js
@@ -1,42 +1,44 @@
 import React, { Component, createContext } from "react";
+import WS from "react-native-websocket";
+
 const UsersContext = createContext();
 
 class UsersProvider extends Component {
   constructor() {
     super();
     this.state = {
-      users: [],
-      socketOpen: false
+      users: []
     };
-    this.socket = null;
   }
 
-  componentDidMount() {
-    this.initSocket();
+  getUserInfoFromData(data) {
+    const match = data.match(/{"results":\[.*\],"info":\{.*\}\}/);
+    if (match) {
+      const json = JSON.parse(match[0]);
+      const user = json.results[0];
+      return user;
+    }
+    return null;
   }
 
-  componentDidUpdate() {
-    if (!this.state.socketOpen) this.initSocket();
-  }
-
-  initSocket() {
-    this.socket = new WebSocket(
-      "wss://wunder-provider.herokuapp.com/socket.io/?EIO=3&transport=websocket"
-    );
-    this.socket.onopen = () => this.setState({ socketOpen: true });
-    this.socket.onclose = () => this.setState({ socketOpen: false });
-    this.socket.onmessage = ({ data }) => {
-      const match = data.match(/{"results":\[.*\],"info":\{.*\}\}/);
-      if (match) {
-        const json = JSON.parse(match[0]);
-        this.setState({ users: [...this.state.users, json.results[0]] });
-      }
-    };
+  addUser(user) {
+    this.setState({ users: [...this.state.users, user] });
   }
 
   render() {
     return (
       <UsersContext.Provider value={this.state.users}>
+        <WS
+          ref={ref => {
+            this.ws = ref;
+          }}
+          url="wss://wunder-provider.herokuapp.com/socket.io/?EIO=3&transport=websocket"
+          onMessage={({ data }) => {
+            const user = this.getUserInfoFromData(data);
+            if (user) this.addUser(user);
+          }}
+          reconnect
+        />
         {this.props.children}
       </UsersContext.Provider>
     );
